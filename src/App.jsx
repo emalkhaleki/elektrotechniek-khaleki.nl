8f163e8de36f4184f7f8dea8bef879fe89f2ff30
emalkhaleki <emalkhaleki@hotmail.com>
2026-04-13 13:01:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 export default function App() {
   const [form, setForm] = useState({
     naam: "",
